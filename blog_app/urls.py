e239dc73f3afb6a492f13714cbd81d72a41437d6
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-03-15 19:29:03 +0100

    added ckeditor to main project

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
     path('users/', include('django.contrib.auth.urls')),
     path('users/', include('users.urls')),
 ]
