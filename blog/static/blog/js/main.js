d13f61802699b293807338621dc92bf74be9d563
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-03-15 18:07:48 +0100

    added event listener to navbar

---
+++ blog/static/blog/js/main.js
@@ -0,0 +1,18 @@
+
+
+
+const nav = document.querySelector('.navbar-nav'),
+    navList = nav.querySelectorAll('li'),
+    totalNavList = navList.length;
+
+console.log(totalNavList)
+
+for (let i = 0; i<totalNavList; i++) {
+    const a = navList[i].querySelector('a');
+    a.addEventListener('click', function() {
+        for (let j = 0; j<totalNavList; j++){
+            navList[j].querySelector('a').classList.remove('active');
+        }
+        this.classList.add('active');
+    })
+}
