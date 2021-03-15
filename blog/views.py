f69d8cdc98378972fcc4b93f5abebb4772b775d4
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-03-15 16:55:50 +0100

    changed fbv to cbv

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-# from django.generic.views import Create
+from django.views.generic import ListView
 
 # Create your views here.
 
 def index(request):
     return render(request, 'blog/index.html')
 
+
+class HomeView(ListView):
+    template_name = 'blog/index.html'
