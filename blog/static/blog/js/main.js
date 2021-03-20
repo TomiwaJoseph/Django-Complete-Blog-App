68071f14b2a8b6651b0f589c5eece0126944de26
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-03-20 19:25:24 +0100

    added scroll to top function

--- blog/static/blog/js/main.js
+++ blog/static/blog/js/main.js
@@ -2,6 +2,13 @@
 
 $(document).ready(function (){
     AOS.init();
+
+    // click to scroll top
+    $('.move-up span').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    })
 })
 
 
