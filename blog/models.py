df778f20b65b286ec9f5ae53f2db565e40771330
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-03-20 19:28:35 +0100

    created the blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
+from django.contrib.auth.models import User
+from ckeditor_uploader.fields import RichTextUploadingField
+from django.utils.text import slugify
 
 # Create your models here.
+class Blog(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+        )
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250,
+        unique_for_date='publish')
+    author = models.ForeignKey(User,
+        on_delete=models.CASCADE,
+        related_name='blog_posts')
+    body = RichTextUploadingField(blank=True, null=True)
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10,
+        choices=STATUS_CHOICES,
+        default='draft')
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Blog, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('my_detail',
+                args=[self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug])
+
