e0a98bde3bb3b0757acb2bcad61123d6dbdfa964
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-03-31 01:28:11 +0100

    added email config for console

--- blog/settings.py
+++ blog/settings.py
@@ -147,3 +147,11 @@ CKEDITOR_CONFIGS = {
         'skin': 'moono',
     },
 }
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get('MAIL_NAME')
+EMAIL_HOST_PASSWORD = os.environ.get('MAIL_PASS')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
