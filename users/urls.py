e4b5c3e0ce7bf207079d0a421f5a21093b9f95e9
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-04-03 23:11:52 +0100

    handled all things profile

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import UserRegister
+from .views import (UserRegister, profile, 
+    edit_profile, author_profile_view,
+    )
 
 
 urlpatterns = [
     path('register/', UserRegister.as_view(), name='register'),
+    path('profile/<slug:user_>/', profile, name='profile'),
+    path('edit-profile/', edit_profile, name='edit_profile'),
+    path('profile/author/<slug:user_>/', author_profile_view, name='view_profile'),
 ]
