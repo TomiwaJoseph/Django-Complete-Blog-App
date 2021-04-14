6b9ee4ca90f48c0c72ae0bd7346cb61ac7e9cd83
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-04-14 20:51:07 +0100

    changed register view

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import (UserRegister, profile, 
-    edit_profile, author_profile_view,
+from .views import (profile, register,
+    edit_profile, author_profile_view, know_user
     )
 
 
 urlpatterns = [
-    path('register/', UserRegister.as_view(), name='register'),
+    path('know_user/', know_user, name='know_user'),
+    path('register/', register, name='register'),
     path('profile/<slug:user_>/', profile, name='profile'),
     path('edit-profile/', edit_profile, name='edit_profile'),
     path('profile/author/<slug:user_>/', author_profile_view, name='view_profile'),
