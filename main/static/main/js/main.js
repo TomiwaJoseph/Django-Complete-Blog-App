d7dfbceb21321551a5b65c70c2bf8c3250bca158
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-04-14 10:53:54 +0100

    made navbar style according to url

--- main/static/main/js/main.js
+++ main/static/main/js/main.js
@@ -8,21 +8,10 @@ $(document).ready(function (){
         $('html, body').animate({
             scrollTop: 0
         }, 1000);
-    })
-})
+    });
 
+    $('li.active').removeClass('active');
+    $('a[href="' + location.pathname + '"]').closest('li').addClass('active');
 
+});
 
-const nav = document.querySelector('.navbar-nav'),
-    navList = nav.querySelectorAll('li'),
-    totalNavList = navList.length;
-
-for (let i = 0; i<totalNavList; i++) {
-    const a = navList[i].querySelector('a');
-    a.addEventListener('click', function() {
-        for (let j = 0; j<totalNavList; j++){
-            navList[j].querySelector('a').classList.remove('active');
-        }
-        this.classList.add('active');
-    })
-}
